b043d15b91d94561964f16f9308c0b86b6787701
malklestat <malklestat@hotmail.com>
2020-03-24 11:42:28 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'deploy',
+    name: 'deployteste',
     script: 'server.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
